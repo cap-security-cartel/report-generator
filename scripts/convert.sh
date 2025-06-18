1e70ba28d648304897f468f9155b07d1b06791c6
0xleastwood <leastsec@proton.me>
2025-06-18 14:53:31 -0600

    fix: removed assisting auditors section

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -6,7 +6,6 @@
 # pandoc with gfm flavored markdown seems to have issues regarding
 # Skipping --from gfm here
 pandoc --filter ./scripts/pandoc-minted.py --filter ./scripts/pandoc-image.py --from gfm ./source/lead_auditors.md -o ./working/lead_auditors.tex
-pandoc --filter ./scripts/pandoc-minted.py --filter ./scripts/pandoc-image.py --from gfm ./source/assisting_auditors.md -o ./working/assisting_auditors.tex
 pandoc --filter ./scripts/pandoc-minted.py --filter ./scripts/pandoc-image.py --from gfm ./source/about_CAPSC.md -o ./working/about_CAPSC.tex
 pandoc --filter ./scripts/pandoc-minted.py --filter ./scripts/pandoc-image.py --from gfm ./source/disclaimer.md -o ./working/disclaimer.tex
 pandoc --filter ./scripts/pandoc-minted.py --filter ./scripts/pandoc-image.py --from gfm ./source/protocol_summary.md -o ./working/protocol_summary.tex
